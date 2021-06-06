8d46ec70b2e6c40e0c95f8241a90732c3297563e
Phil Brockman <phil.brockman@gmail.com>
2021-06-06 17:05:35 -0400

    Updates

--- client/src/components/Defender.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {UnitSelector} from "./UnitSelector/UnitSelector"
-
-export const Defender = ({unit, setUnit, units}) => {
-    return <UnitSelector setUnit={setUnit} currentUnit={unit} units={units} showTags={true}>
-    </UnitSelector>
-  }
-        
